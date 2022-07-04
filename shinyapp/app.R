8d4728825385cfdf29667040f535cf2200859c7a
Nandini <nandinidas@vt.edu>
2022-07-03 20:16:46 -0400

    shinyapp edits

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -99,24 +99,61 @@ map1<-leaflet(data = map) %>% addTiles() %>%
                                                                                                                     fillOpacity = 0.5) %>%
   addMarkers(~Longitude, ~Latitude, popup = ~as.character(Address), label = ~as.character(School)) 
 
+#---------Age pie chart---------------------------------------
 
-#baseline <- livdiv %>%
-  #slice(1:307,)
+this.year = 2020
+vars <- load_variables(year = 2020,
+                       dataset = "acs5",
+                       cache = TRUE)
+dim(vars)
 
-# participate in ag data 
+sterling <- read_excel(paste0(getwd(),"/data/sterlingdata.xlsx"),skip=1,col_names=TRUE)
+subset_sterling <- sterling[6:18,c(1, 10:13)]
+subset_sterling
+sterling$Percent...12[6:18]
+age_estimate <- sterling$Percent...12[6:18]
+age_estimate <- gsub("%","",age_estimate)
+age_percent <- as.numeric(age_estimate)
+age_cat <- (subset_sterling$Label)
+#this is how to subset the data
+#turn categories into factors 
+age<-ggplot(subset_sterling,aes(x=age_cat,y=age_percent, fill=age_cat))+geom_col()+theme(axis.text.x=element_blank(), axis.title.x = element_blank(), axis.text.y = element_blank(),axis.ticks.y=element_blank())+scale_x_discrete(limits=age_cat)+labs(caption= "Source: S0101 ACS 5-year data 2016-2020",y="Percent",)+ coord_polar()+guides(fill = guide_legend(title = "Age Group")) + geom_text(aes(label = age_percent, y = age_percent), size = 3, position = position_stack(vjust = 0.8))
 
-#grouped <- baseline %>% group_by(village) %>% summarize(prop_farm = sum(farm_yn)/n())
+#-------income--------------------------------
+medianin <- read_excel(paste0(getwd(),"/data/incomemedian.xlsx"))
+
+subset_medianin <- medianin[4:13, 1:5]
+subset_medianin
+mi_pop <- subset_medianin$...4
+mi_cat <- subset_medianin$...1
 
 
-# remmitences v income data 
+mi_cat.fac <- factor(mi_cat, levels = c(mi_cat))
+subset_medianin$new_pop<-gsub("%$","",subset_medianin$...4)
+pop_nop <- subset_medianin$new_pop
+pop_num <- as.numeric(pop_nop)
+income <- ggplot(subset_medianin,aes(x=mi_cat.fac,y=pop_num, fill=mi_cat.fac))+geom_col(stat="identity")+theme(axis.text.x=element_blank(),axis.ticks.y=element_blank(),axis.title.x = element_blank()+scale_x_discrete(limits=mi_cat.fac),axis.text.y=element_blank())+labs(caption= "Source: S1901 ACS 5-year data 2016-2020",x="Income", y="Percent") + coord_polar() + guides(fill = guide_legend(title = "Income Level ($)")) + geom_text(aes(label=pop_num,y=pop_num), size = 3, position = position_stack(vjust = 1.1))
 
-#baseline.summary <- baseline %>% select(village, exp_total, full_inc, rmt_total) %>% group_by(village) %>%summarise_all(mean) 
 
-#land owned data 
-#villages <- c("Amrabati","Beguakhali","Bijoynagar","Birajnagar","Haridaskati Samsernagar","Lakshmi Janardanpur","Pargumti","Purba Dwarokapur","Sagar","Shibpur") 
+#------------------poverty-------------------------------
 
-# savings data 
-#--------------------------------------------------------------------
+poverty_as<- read_excel(paste0(getwd(),"/data/povertybyageandsexnewss.xlsx"), 
+           sheet = "Data")
+
+subset_poverty_as <- poverty_as[3:28, 1:4]
+subset_poverty_as
+#subset_poverty_as$Estimate[1:26]
+povas_pop <- subset_poverty_as$Estimate
+povas_pop <- as.numeric(povas_pop)
+povas_cat <- subset_poverty_as$Label
+
+ggplot(subset_poverty_as, aes(x=povas_cat,y=povas_pop,fill=Sex)) + 
+  geom_bar(stat = "identity",position=position_dodge(),width=.6)   + coord_flip() + theme(axis.text.x=element_text(angle=90)) +scale_fill_manual(values=c("tomato2", "darkblue"))+labs(title="Poverty by Age and Sex in Sterling, VA",subtitle="2020",caption= "Source: ACS data 2016-2020",x="Age",y="Estimated Population") +scale_x_discrete(limits=c("Under 5 years", "5 years","6 to 11 years","12 to 14 years","15 years","16 to 17 years","18 to 24 years","25 to 34 years","35 to 44 years","45 to 54 years","55 to 64 years","65 to 74 years","75 years and older")) 
+
+
+pov <- ggplot(subset_poverty_as,aes(x=povas_cat,y=povas_pop, fill=Sex))+geom_col(position="dodge",width=1.5)+theme(axis.text.x=element_text(angle=90, size=7.5, face="bold"),axis.title.x = element_blank())+scale_x_discrete(limits=povas_cat[1:length(povas_cat)/2])+labs(caption= "Source: B17001 ACS 5-year data 2016-2020",x="Age",y="Total Population")+ scale_fill_discrete(name = "", labels = c("Female", "Male")) 
+
+
 
 # CODE TO DETECT ORIGIN OF LINK AND CHANGE LOGO ACCORDINGLY
 jscode <- "function getUrlVars() {
@@ -222,11 +259,72 @@ ui <- navbarPage(title = "DSPG-LivDiv 2022",
                                    #    p(tags$small(em('Last updated: August 2021'))))
                                   ) 
                                    )
-                 )
+                 ), 
+                 navbarMenu("Sociodemographics" , 
+                            tabPanel("Sterling", 
+                                     fluidRow(style = "margin: 6px;",
+                                              h1(strong("Sterling"), align = "center"),
+                                              p("", style = "padding-top:10px;"), 
+                                              #column(4, 
+                                              #      h4(strong("Education")),
+                                              #     p("These are demographics"),
+                                              #) ,
+                                              column(12, 
+                                                     h4(strong("Sterling, CDP")),
+                                                     selectInput("demosdrop", "Select Variable:", width = "60%", choices = c(
+                                                       "Age" = "age",
+                                                       "Gender" = "gender",
+                                                       "Race/ethnicity" = "race", 
+                                                       "Educational Attainment" = "edu",
+                                                       "Poverty by Age and Sex" = "pov", 
+                                                       "Marital Status" = "mar",
+                                                       "Family Income" = "faminc",
+                                                       "Property Value" = "propval"
+                                                       ),
+                                                     ), 
+                                                     withSpinner(plotOutput("ageplot", height = "500px", width = "60%")),
+                                                     
+                                              ),
+                                              # column(12, 
+                                              #      h4("References: "), 
+                                              #     p(tags$small("[1] Groundwater: Groundwater sustainability. (2021). Retrieved July 27, 2021, from https://www.ngwa.org/what-is-groundwater/groundwater-issues/groundwater-sustainability")) ,
+                                              #     p("", style = "padding-top:10px;")) 
+                                     )), 
+                            tabPanel("Community Schools", 
+                                     fluidRow(style = "margin: 6px;",
+                                              h1(strong("Demographics of Community Schools"), align = "center"),
+                                              p("", style = "padding-top:10px;"), 
+                                              #column(4, 
+                                              #      h4(strong("Education")),
+                                              #     p("These are demographics"),
+                                              #  ) ,
+                                              column(12, 
+                                                     h4(strong("Community Schools")),
+                                                     selectInput("schooldrop", "Select Variable:", width = "60%", choices = c(
+                                                       "Gender" = "cgender",
+                                                       "Race/Ethnicity" ="crace", 
+                                                       "Hispanic Population" = "chispanic",
+                                                       "No. of teacher" = "teacher",
+                                                       "Enrollment" = "enrol", 
+                                                       "Absences By Quarter" = "absense", 
+                                                       "Chronic Absenteeism" = "chronic"
+                                                     ),
+                                                     ), 
+                                                     withSpinner(plotOutput("ocuplot", height = "500px")),
+                                                     
+                                              ),
+                                              # column(12, 
+                                              #       h4("References: "), 
+                                              #       p(tags$small("[1] Groundwater: Groundwater sustainability. (2021). Retrieved July 27, 2021, from https://www.ngwa.org/what-is-groundwater/groundwater-issues/groundwater-sustainability")) ,
+                                              #      p("", style = "padding-top:10px;")) 
+                                     )), 
+                            
+                            
+                            
+                 ), 
+                 
 )
                  
-                 
-                 ## Sundarbans Region--------------------------------------------
                  
 # server -----------------------------------------------------------
 server <- function(input, output, session) {
@@ -238,6 +336,23 @@ server <- function(input, output, session) {
     map1
   })
   
+  ageVar <- reactive({
+    input$demosdrop
+  })
+  
+  output$ageplot <- renderPlot({
+    if (ageVar() == "age") {
+      
+      age
+    }
+    else if (ageVar() == "faminc") {
+     income
+    }
+    else if(ageVar() == "pov"){
+      pov
+    }
+    
+  })
 }
   
   #sociodemo tabset ----------------------------------------------------
