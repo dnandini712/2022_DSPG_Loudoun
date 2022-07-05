aad1744b6269d31371fec65a505d361d1b12126a
Abdullah <abdullahrizwan@vt.edu>
2022-07-05 09:20:45 -0400

    Shiny app

---
+++ Shinyapp/app.R
@@ -0,0 +1,65 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+library(dplyr)
+library(tidycensus)
+library(ggplot2)
+library(mapdeck)
+library(shiny)
+library(shinythemes)
+library(shinyjs)
+library(ggplot2)
+library(maps)
+library(plotly)
+library(DT)
+library(tigris)
+library(tidyverse)
+library(tidycensus)
+library(readxl)
+library(collapsibleTree)
+library(shinycssloaders)
+library(leaflet)
+library(leaflet.extras)
+library(rvest)
+library(sf)
+library(shinydashboard)
+library(shinydashboardPlus)
+library(tidygeocoder)
+library(janitor)
+library(ggplot2)
+library(magrittr)
+options(tigris_use_cache = TRUE)
+library(ggwordcloud)
+library(ggthemes)
+library(RColorBrewer)
+library(sjmisc)
+library(shinythemes)
+library(DT)
+library(data.table)
+library(rsconnect)
+library(readr)
+library(stringr)
+library(mapdata)
+library(htmlwidgets)
+library(leafpop)
+library(lattice)
+library(htmltools)
+library(leaflegend)
+library(ggplotify)
+library(grid)
+library(gridExtra)
+library(ggpubr)
+library(lubridate)
+library(shinyWidgets)
+library(viridis)
+
+
+
