66c33545b3f486c0af3cc3ae714a67323414f4cc
Jontayvion Osborne <tayosborne@Tays-MacBook-Air.local>
2022-07-05 10:18:54 -0400

    idk

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -1,4 +1,3 @@
-# Load Packages ---------------------------------------------------------------
 library(dplyr)
 library(tidycensus)
 library(ggplot2)
@@ -59,6 +58,10 @@ options(spinner.color = prettyblue, spinner.color.background = '#ffffff', spinne
 colors <- c("#232d4b","#2c4f6b","#0e879c","#60999a","#d1e0bf","#d9e12b","#e6ce3a","#e6a01d","#e57200","#fdfdfd")
 
 
+# data -----------------------------------------------------------
+
+# Sterling Map ----------------------------------------------------
+
 
 readRenviron("~/.Renviron")
 Sys.getenv("CENSUS_API_KEY")
@@ -95,6 +98,59 @@ map1<-leaflet(data = map) %>% addTiles() %>%
                                                                                                                     fillOpacity = 0.5) %>%
   addMarkers(~Longitude, ~Latitude, popup = ~as.character(Address), label = ~as.character(School)) 
 
+#---------Age pie chart---------------------------------------
+
+this.year = 2020
+vars <- load_variables(year = 2020,
+                       dataset = "acs5",
+                       cache = TRUE)
+dim(vars)
+
+sterling <- read_excel(paste0(getwd(),"/data/sterlingdata.xlsx"),skip=1,col_names=TRUE)
+subset_sterling <- sterling[6:18,c(1, 10:13)]
+subset_sterling
+sterling$Percent...12[6:18]
+age_estimate <- sterling$Percent...12[6:18]
+age_estimate <- gsub("%","",age_estimate)
+age_percent <- as.numeric(age_estimate)
+age_cat <- (subset_sterling$Label)
+#this is how to subset the data
+#turn categories into factors 
+age<-ggplot(subset_sterling,aes(x=age_cat,y=age_percent, fill=age_cat))+geom_col()+theme(axis.text.x=element_blank(), axis.title.x = element_blank(), axis.text.y = element_blank(),axis.ticks.y=element_blank())+scale_x_discrete(limits=age_cat)+labs(caption= "Source: S0101 ACS 5-year data 2016-2020",y="Percent",)+ coord_polar()+guides(fill = guide_legend(title = "Age Group")) + geom_text(aes(label = age_percent, y = age_percent), size = 3, position = position_stack(vjust = 0.8))
+
+#-------income--------------------------------
+medianin <- read_excel(paste0(getwd(),"/data/incomemedian.xlsx"))
+
+subset_medianin <- medianin[4:13, 1:5]
+subset_medianin
+mi_pop <- subset_medianin$...4
+mi_cat <- subset_medianin$...1
+
+
+mi_cat.fac <- factor(mi_cat, levels = c(mi_cat))
+subset_medianin$new_pop<-gsub("%$","",subset_medianin$...4)
+pop_nop <- subset_medianin$new_pop
+pop_num <- as.numeric(pop_nop)
+income <- ggplot(subset_medianin,aes(x=mi_cat.fac,y=pop_num, fill=mi_cat.fac))+geom_col(stat="identity")+theme(axis.text.x=element_blank(),axis.ticks.y=element_blank(),axis.title.x = element_blank()+scale_x_discrete(limits=mi_cat.fac),axis.text.y=element_blank())+labs(caption= "Source: S1901 ACS 5-year data 2016-2020",x="Income", y="Percent") + coord_polar() + guides(fill = guide_legend(title = "Income Level ($)")) + geom_text(aes(label=pop_num,y=pop_num), size = 3, position = position_stack(vjust = 1.1))
+
+
+#------------------poverty-------------------------------
+
+poverty_as<- read_excel(paste0(getwd(),"/data/povertybyageandsexnewss.xlsx"), 
+                        sheet = "Data")
+
+subset_poverty_as <- poverty_as[3:28, 1:4]
+subset_poverty_as
+#subset_poverty_as$Estimate[1:26]
+povas_pop <- subset_poverty_as$Estimate
+povas_pop <- as.numeric(povas_pop)
+povas_cat <- subset_poverty_as$Label
+
+ggplot(subset_poverty_as, aes(x=povas_cat,y=povas_pop,fill=Sex)) + 
+  geom_bar(stat = "identity",position=position_dodge(),width=.6)   + coord_flip() + theme(axis.text.x=element_text(angle=90)) +scale_fill_manual(values=c("tomato2", "darkblue"))+labs(title="Poverty by Age and Sex in Sterling, VA",subtitle="2020",caption= "Source: ACS data 2016-2020",x="Age",y="Estimated Population") +scale_x_discrete(limits=c("Under 5 years", "5 years","6 to 11 years","12 to 14 years","15 years","16 to 17 years","18 to 24 years","25 to 34 years","35 to 44 years","45 to 54 years","55 to 64 years","65 to 74 years","75 years and older")) 
+
+
+pov <- ggplot(subset_poverty_as,aes(x=povas_cat,y=povas_pop, fill=Sex))+geom_col(position="dodge",width=1.5)+theme(axis.text.x=element_text(angle=90, size=7.5, face="bold"),axis.title.x = element_blank())+scale_x_discrete(limits=povas_cat[1:length(povas_cat)/2])+labs(caption= "Source: B17001 ACS 5-year data 2016-2020",x="Age",y="Total Population")+ scale_fill_discrete(name = "", labels = c("Female", "Male")) 
 
 
 
@@ -135,7 +191,45 @@ jscode <- "function getUrlVars() {
              //changeLinks('economic');
            }
            "
-ui <- navbarPage(title = "Loudoun County Project 2022",
+
+
+jscode <- "function getUrlVars() {
+                var vars = {};
+                var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+                    vars[key] = value;
+                });
+                return vars;
+            }
+           function getUrlParam(parameter, defaultvalue){
+                var urlparameter = defaultvalue;
+                if(window.location.href.indexOf(parameter) > -1){
+                    urlparameter = getUrlVars()[parameter];
+                    }
+                return urlparameter;
+            }
+            var mytype = getUrlParam('type','Empty');
+            function changeLinks(parameter) {
+                links = document.getElementsByTagName(\"a\");
+                for(var i = 0; i < links.length; i++) {
+                   var link = links[i];
+                   var newurl = link.href + '?type=' + parameter;
+                   link.setAttribute('href', newurl);
+                 }
+            }
+           var x = document.getElementsByClassName('navbar-brand');
+           if (mytype != 'economic') {
+             x[0].innerHTML = '<div style=\"margin-top:-14px\"><a href=\"https://datascienceforthepublicgood.org/node/451\">' +
+                              '<img src=\"DSPG_black-01.png\", alt=\"DSPG 2020 Symposium Proceedings\", style=\"height:42px;\">' +
+                              '</a></div>';
+             //changeLinks('dspg');
+           } else {
+             x[0].innerHTML = '<div style=\"margin-top:-14px\"><a href=\"https://datascienceforthepublicgood.org/economic-mobility/community-insights/case-studies\">' +
+                              '<img src=\"AEMLogoGatesColorsBlack-11.png\", alt=\"Gates Economic Mobility Case Studies\", style=\"height:42px;\">' +
+                              '</a></div>';
+             //changeLinks('economic');
+           }
+           "
+ui <- navbarPage(title = "DSPG 2022",
                  selected = "overview",
                  theme = shinytheme("lumen"),
                  tags$head(tags$style('.selectize-dropdown {z-index: 10000}')),
@@ -266,7 +360,6 @@ ui <- navbarPage(title = "Loudoun County Project 2022",
                  
 )
 
-
 # server -----------------------------------------------------------
 server <- function(input, output, session) {
   # Run JavaScript Code
@@ -296,7 +389,5 @@ server <- function(input, output, session) {
   })
 }
 
-#sociodemo tabset ----------------------------------------------------
-
 shinyApp(ui = ui, server = server)
-  
+
