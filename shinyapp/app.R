3ba9990d91049d29f36e1b5c7d5c630fdcefa3a1
amanda ljuba <amandal01@vt.edu>
2022-07-27 09:29:21 -0400

    fixed student teacher ratio

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -1759,7 +1759,7 @@ ui <- navbarPage(title = "DSPG",
                                               
                                      )), 
                             
-                            tabPanel("Education", 
+                            tabPanel("Performance", 
                                      fluidRow(style = "margin: 6px;",
                                               column(12, 
                                                      h1(strong("Education"), align = "center")),
@@ -1775,7 +1775,7 @@ ui <- navbarPage(title = "DSPG",
                                             ),
                                             
                                             withSpinner(plotlyOutput("ocuplot2", height = "500px", width = "100%")),
-                                            img(src = "StudentTeacherRatioPic.png", class = "topimage", width = "70%", height ="70%", style = "display: block; margin-left: auto; margin-right: auto;"),
+                                            img(src = "stratio.png", class = "topimage", width = "80%", height ="80%", style = "display: block; margin-left: auto; margin-right: auto;"),
 
                                             column(12,align = "right",
                                                    p("Source: Virginia Department of Education, Loudoun County Public Schools Dashboard and Staff directory", style = "font-size:12px;"),
